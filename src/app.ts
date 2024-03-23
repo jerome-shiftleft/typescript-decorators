1292ed013b426fd174ee745b711c6cc5951952f9
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-23 17:39:03 +0800

    more useful decorators initial setup

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 //import firstClassDecorator from "./lectures/first-class-decorator";
-import decoratorFactories from "./lectures/decorator-factories";
+//import decoratorFactories from "./lectures/decorator-factories";
+import moreUsefulDecorators from "./lectures/more-useful-decorators";
 
 export default function app() {  
-  decoratorFactories();
+  moreUsefulDecorators();
 } 
---
+++ src/lectures/more-useful-decorators.ts
@@ -0,0 +1,21 @@
+export default function moreUsefulDecorators() {
+  function Logger(logString: string) {
+    return function (constructor: Function) {
+      console.log(logString);
+      console.log(constructor);
+    };
+  }
+
+  @Logger("LOGGING - PERSON")
+  class Person {
+    name = "Max";
+
+    constructor() {
+      console.log("Creating person object...");
+    }
+  }
+
+  const pers = new Person();
+
+  console.log(pers);
+} // end of export default function moreUsefulDecorators()
