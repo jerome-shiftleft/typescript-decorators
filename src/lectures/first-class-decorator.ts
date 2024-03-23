55c1cefc24da6107cb4cca3152857e5b633a8b27
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-23 17:30:39 +0800

    Add Logger decorator to Person class

--- src/lectures/first-class-decorator.ts
+++ src/lectures/first-class-decorator.ts
@@ -1,3 +1,21 @@
 export default function firstClassDecorator() {
 
+  function Logger(constructor: Function) {
+    console.log('Logging...')
+    console.log(constructor);
+  }
+
+  @Logger
+  class Person {
+    name = 'Max';
+
+    constructor() {
+      console.log('Creating person object...');
+    }
+  }
+
+  const pers = new Person();
+
+  console.log(pers);
+
 } // end of export default function firstClassDecorator()
